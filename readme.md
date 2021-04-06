0fed5749e20842f55c8c0551f0a924a9bb13f9b9
jarbasal <jarbasai@mailfence.com>
2021-04-06 02:02:56 +0100

    unittests

---
+++ readme.md
@@ -0,0 +1,26 @@
+# Pronomial
+
+simple and fast coreference solver, uses pos tags, a word gender classifier 
+and lists of pronouns
+
+Supported languages: English, Portuguese
+
+# Install
+
+```bash
+pip install pronomial
+```
+
+# Usage
+
+```python
+from pronomial import replace_corefs
+
+replace_corefs("London has been a major settlement for two millennia. "
+               "It was founded by the Romans, who named it Londinium.", 
+               lang="en")
+"""
+"London has been a major settlement for two millennia . "
+"London was founded by the Romans , Romans named London Londinium ."
+"""
+```
--- test/test_pt.py
+++ test/test_pt.py
@@ -21,6 +21,12 @@ class TestCorefPT(unittest.TestCase):
 
     def test_plural(self):
         self.assertEqual(
+            replace_corefs("As mulheres da ribeira do sado é que é, "
+                           "Elas lavram a terra com as unhas dos pés",
+                           lang="pt"),
+            "As mulheres da ribeira do sado é que é , mulheres lavram a terra com as unhas dos pés"
+        )
+        self.assertEqual(
             replace_corefs("Os americanos foram á lua. Eles são fodidos",
                            lang="pt"),
             "Os americanos foram á lua . americanos são fodidos"
